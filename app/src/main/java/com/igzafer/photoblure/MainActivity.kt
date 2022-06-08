2b8dcba797abd9baab68fa6bef38f90401f38f95
Zafer <zaferkurumsal@gmail.com>
2022-06-08 20:50:38 +0300

    fotoğraf kalite düzenlemesi

--- app/src/main/java/com/igzafer/photoblure/MainActivity.kt
+++ app/src/main/java/com/igzafer/photoblure/MainActivity.kt
@@ -5,19 +5,24 @@ import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
+import android.util.Log
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.igzafer.photoblure.databinding.ActivityMainBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import java.io.File
 
 
 class MainActivity : AppCompatActivity() {
@@ -32,59 +37,58 @@ class MainActivity : AppCompatActivity() {
         viewModel = ViewModelProvider(this)[HomeViewModel::class.java]
 
         viewModel.currentBitmap.observe(this, Observer { it ->
-            val width: Int = it.width
-            val height: Int = it.height
-            val pixels = IntArray(width * height)
-            it.getPixels(pixels, 0, width, 0, 0, width, height);
-            if (it != null) {
-                CoroutineScope(Dispatchers.IO).launch {
-                    val alpha = 0xFF shl 24 // ?bitmap?24?
-                    for (i in 0 until width) {
-                        for (j in 0 until height) {
-                          for (mi in 0..3){
-                              for (mj in 0..3){
+            binding.ImageView.setImageBitmap(it)
+            //burayı sen ekle üsendim
 
-                              }
-                          }
-                        }
-                    }
-                    CoroutineScope(Dispatchers.Main).launch {
-                        val newBmp = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565)
-                        newBmp.setPixels(pixels, 0, width, 0, 0, width, height)
-                        binding.ImageView.setImageBitmap(newBmp)
-                    }
-                }
-
-            }
         })
         binding.takePicture.setOnClickListener {
             if (ContextCompat.checkSelfPermission(
                     this,
                     Manifest.permission.CAMERA
                 ) != PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ) != PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                ) != PackageManager.PERMISSION_GRANTED
             ) {
-                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), 100)
-                if (ContextCompat.checkSelfPermission(
-                        this,
-                        Manifest.permission.CAMERA
-                    ) == PackageManager.PERMISSION_GRANTED
-                ) {
-                    val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                    startForResult.launch(Intent(takePictureIntent))
-                }
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(
+                        Manifest.permission.CAMERA,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.READ_EXTERNAL_STORAGE
+                    ),
+                    100
+                )
             } else {
+                val fileName = "photo";
+                val storageDirectory = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+                val imageFile = File.createTempFile(fileName, ".jpg", storageDirectory)
                 val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                startForResult.launch(Intent(takePictureIntent))
+                currentPhotoPath = imageFile.absolutePath
+                val uri = FileProvider.getUriForFile(
+                    applicationContext,
+                    "com.igzafer.photoblure",
+                    imageFile
+                )
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, uri)
+                startForResult.launch(takePictureIntent)
+
             }
 
         }
     }
 
+    private var currentPhotoPath: String = ""
     private val startForResult =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
             if (result.resultCode == Activity.RESULT_OK) {
-                val intent = result.data!!
-                val imageBitmap = intent.extras!!.get("data") as Bitmap
+                val options = BitmapFactory.Options()
+                val imageBitmap = BitmapFactory.decodeFile(currentPhotoPath, options)
                 viewModel.currentBitmap.value = imageBitmap
             }
         }
