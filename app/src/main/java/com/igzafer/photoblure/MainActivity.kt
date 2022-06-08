92c1ef4e00eb4659d8a5c969cd9ca413f46d783c
Ahmet Tarık Duyar <47754855+Atduyar@users.noreply.github.com>
2022-06-08 22:55:08 +0300

    Update MainActivity.kt

--- app/src/main/java/com/igzafer/photoblure/MainActivity.kt
+++ app/src/main/java/com/igzafer/photoblure/MainActivity.kt
@@ -38,7 +38,66 @@ class MainActivity : AppCompatActivity() {
 
         viewModel.currentBitmap.observe(this, Observer { it ->
             binding.ImageView.setImageBitmap(it)
-            //burayı sen ekle üsendim
+            val offset = 1;
+            var kernel = floatArrayOf(
+                1.0f/9.0f, 1.0f/9.0f, 1.0f/9.0f,
+                1.0f/9.0f, 1.0f/9.0f, 1.0f/9.0f,
+                1.0f/9.0f, 1.0f/9.0f, 1.0f/9.0f);
+
+            val width: Int = it.width
+            val height: Int = it.height
+            val pixels = IntArray(width * height)
+            val newPixels = IntArray(width * height)
+            it.getPixels(pixels, 0, width, 0, 0, width, height);
+            for (i in 0 until width * height){
+                newPixels[i] = 0;
+            }
+            if (it != null) {
+                CoroutineScope(Dispatchers.IO).launch {
+
+                    for (i in 0 until width) {
+                        for (j in 0 until height) {
+                            var resultP = IntArray(3);
+                            resultP[0] = 0;//red
+                            resultP[1] = 0;//green
+                            resultP[2] = 0;//blue
+                            for (mi in (-offset)..(offset)){
+                                for (mj in (-offset)..(offset)){
+                                    val resultPixel = pixels[(i+mi) * width + (j+mj)]
+                                    // int değeri 4 byte yer kaplar
+                                    // rgba daki her bir değer (red) 1byte yer kaplar
+                                    // yani 1 int in içine 0-255 e kadar olan rgba değerleri atanabilir
+                                    // bu int in içinde sadece r(red) yi almak istersek 3. byte hariç tüm bitleri sıfırlamamız
+                                    // sonra 3. byte nin 1. byte nin yerine kadar kaydırmamız gerekir
+                                    // *Pixel and 0x00ff0000* 3. byte hariç dier byte leri sıfırlar
+                                    // *shr 16* 16 bit(2 byte) sağ doğru kaydırı
+
+                                    val red = resultPixel and 0x00ff0000 shr 16;
+                                    val green = resultPixel and 0x0000ff00 shr 8;
+                                    val blue = resultPixel and 0x000000ff;
+                                    var karnelIndex = (mi+offset)*((offset*2)+1)+(mj+offset)
+                                    resultP[0] = (red * kernel[karnelIndex]).toInt();
+                                }
+                            }
+                            for (k in 0 until resultP.size) {
+                                if(resultP[i] < 0) resultP[i] = 0;
+                                if(resultP[i] > 255) resultP[i] = 255;
+                            }
+                            var rgbToInt:Int = 255 shl 24;
+                            rgbToInt += resultP[0] shl 16;
+                            rgbToInt += resultP[1] shl 8;
+                            rgbToInt += resultP[3];
+                            newPixels[i * width + j] = resultP[rgbToInt];
+                        }
+                    }
+                    CoroutineScope(Dispatchers.Main).launch {
+                        val newBmp = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565)
+                        newBmp.setPixels(newPixels, 0, width, 0, 0, width, height)
+                        binding.ImageView.setImageBitmap(newBmp)
+                    }
+                }
+
+            }
 
         })
         binding.takePicture.setOnClickListener {
